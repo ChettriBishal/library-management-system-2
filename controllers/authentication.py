deb766f721bfb0ac29c37d7327abff7b1c083e96
Bishal Chettri <bishalin01@gmail.com>
2023-10-29 13:36:16 +0530

    login

--- controllers/authentication.py
+++ controllers/authentication.py
@@ -1,8 +1,33 @@
+from bcrypt import checkpw
+
+from utils import sql_query
+from models.database import get_item
+
+
 class Authentication:
 
+    def __init__(self, username, password, role):
+        self.username = username
+        self.password = password
+        self.role = role
+
+    def _check_password(self, hashed_password):
+        if checkpw(self.password, hashed_password):
+            return True
+        return False
+
     def login(self):
-        pass
+        check_user = get_item(sql_query.GET_USER_BY_USERNAME, (self.username,))
+        if check_user is None:
+            print(f"User with username `{self.username}` does not exist!")
+        else:
+            stored_hashed_password = check_user[2]
+
+            if self._check_password(stored_hashed_password):
+                print(f"Logged in as `{self.role}` successfully!!")
+                return check_user
+            else:
+                return None
 
     def signup(self):
         pass
-
---
+++ utils/sql_query.py
@@ -0,0 +1,66 @@
+GET_USER_BY_USERNAME = f"""
+SELECT * FROM users WHERE username = ?
+"""
+
+GET_PASSWORD = f"""
+SELECT password FROM users WHERE username = ?
+"""
+
+SELECT_ROLE = """
+SELECT role FROM users WHERE username = ?
+"""
+
+GET_ALL_USERS = """
+SELECT username, role FROM users
+"""
+
+CREATE_USER_TABLE = """
+CREATE TABLE IF NOT EXISTS users(user_id text primary key, username text,password text, role text)
+"""
+
+ADD_USER = """
+INSERT INTO users VALUES(?,?,?,?)
+"""
+
+GET_USER = """
+SELECT * FROM users WHERE username=?
+"""
+
+REMOVE_USER = """
+DELETE FROM users WHERE username = ?
+"""
+
+CREATE_BOOK_ISSUE_TABLE = f"""CREATE TABLE IF NOT EXISTS books_issue(
+            issue_id TEXT PRIMARY KEY, 
+            username TEXT,  -- Added username column
+            book_id TEXT, 
+            issue_date DATE NOT NULL, 
+            due_date DATE NOT NULL, 
+            date_returned DATE, 
+            FOREIGN KEY (book_id) REFERENCES books (book_id),
+            FOREIGN KEY (username) REFERENCES users (username)  -- Added foreign key for user_id
+        )"""
+
+ISSUE_BOOK_QUERY = """
+INSERT INTO books_issue (issue_id, username, book_id, issue_date, due_date, date_returned)
+VALUES (?, ?, ?, ?, ?, ?)
+"""
+
+BOOK_ISSUED = """
+SELECT books.name, books.author, books_issue.issue_date, books_issue.due_date, books_issue.date_returned
+FROM books_issue
+INNER JOIN books ON books_issue.book_id = books.book_id
+WHERE books_issue.username = ?
+"""
+
+ISSUE_ID = """
+SELECT books_issue.issue_id
+FROM books_issue
+INNER JOIN books ON books_issue.book_id = books.book_id
+WHERE books_issue.username = ?
+AND books.name = ?
+"""
+
+BOOK_RETURN = """
+DELETE FROM books_issue WHERE issue_id = ?
+"""
