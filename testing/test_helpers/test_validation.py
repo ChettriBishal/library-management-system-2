387971f4564989800540817d117bd338cfde914e
Bishal Chettri <bishalin01@gmail.com>
2023-12-16 21:53:50 +0530

    validation tested

--- testing/test_helpers/test_validation.py
+++ testing/test_helpers/test_validation.py
@@ -2,4 +2,27 @@ from src.helpers import validation
 
 
 class TestValidation:
-    pass
+    def test_validate_username_invalid(self):
+        username = '#@!@#@'  # which is invalid
+        res = validation.validate_username(username)
+
+        assert not res
+
+    def test_validate_username_valid(self):
+        username = 'test_123'
+        res = validation.validate_username(username)
+
+        assert res
+
+    def test_validate_password_invalid(self):
+        password = '12312'
+        res = validation.validate_password(password)
+
+        assert not res
+
+    def test_validate_password_valid(self):
+        password = 'StrongPassword@1231*'
+        res = validation.validate_password(password)
+
+        assert res
+
