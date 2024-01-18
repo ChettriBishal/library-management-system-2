c93cc08010a1d9faf17a8d2d2516d196479478a1
Bishal Chettri <bishalin01@gmail.com>
2024-01-18 10:37:55 +0530

    validation added

--- src/routes/visitor_routes.py
+++ src/routes/visitor_routes.py
@@ -1,23 +1,27 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from src.controllers.user import Visitor
+from src.schemas import BookNameSchema
 
 blp = Blueprint('visitor', __name__, description='operations performed by a visitor')
 
 
-@blp.route('/issues/<string:name>')
+@blp.route('/issues/')
 class IssueBook(MethodView):
-    def post(self, name):
-        issued = Visitor().issue_book(name)
+    @blp.arguments(BookNameSchema)
+    def post(self, data):
+        issued = Visitor().issue_book(data["name"])
         if issued:
-            return {"message": f"Book {name} issued successfully!"}, 200
-        abort(404, message='Book not found')
+            return {"message": f"Book {data['name']} issued successfully!"}, 200
+        else:
+            abort(404, message='Book not found')
 
 
-@blp.route('/issues/<string:username>')
+@blp.route('/issues')
 class BooksIssued(MethodView):
-    def get(self, username):
+    def get(self):
         issued = Visitor().books_issued()
         if issued:
             return {"books": issued}, 200
-        abort(404, message="Books not issued")
+        else:
+            abort(404, message="Books not issued")
--- src/schemas.py
+++ src/schemas.py
@@ -1,14 +1,21 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 
 class UserSchema(Schema):
-    username = fields.Str(required=True)
-    password = fields.Str(required=True, load_only=True)  # never return the user's password
-    role = fields.Str(required=True)
+    username = fields.Str(required=True, validate=validate.Regexp("[A-Za-z1-9_]+"))
+    password = fields.Str(required=True, load_only=True, validate=validate.Regexp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?["
+                                                                                  "0-9])(?=.*?[#?!@$%^&*-]).{8,"
+                                                                                  "}$"))  # never return the user's
+    # password
+    role = fields.Str(required=True, validate=validate.Regexp("^[a-zA-Z]+$"))
 
 
 class UserRegisterSchema(UserSchema):
-    email = fields.Str(required=True)
+    email = fields.Str(required=True, validate=validate.Regexp("^[a-zA-Z][a-zA-Z0-9]+\@[a-zA-Z]+\.(in|net|com)"))
+
+
+class BookNameSchema(Schema):
+    name = fields.Str(required=True)
 
 
 class BookSchema(Schema):
