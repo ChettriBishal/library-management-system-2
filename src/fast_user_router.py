8584b3a8e132ac8fdda215bed89435b82fae1a11
Bishal Chettri <bishalin01@gmail.com>
2024-01-23 11:53:37 +0530

    logout fast

--- src/fast_user_router.py
+++ src/fast_user_router.py
@@ -11,3 +11,11 @@ async def login_user(user_data=Body()):
     if logged_in:
         return {"message": f"{user_data['username']} has logged in!"}
     return {"message": f"{user_data['username']} not found!"}
+
+
+@app.post('/signup')
+async def signup_user(user_data=Body()):
+    auth = Authentication(user_data['username'], user_data['password'], user_data['role'])
+    signed_up = auth.signup()
+    if signed_up:
+        return {"message": f"{user_data['username']} signed up, uuid {signed_up}"}
